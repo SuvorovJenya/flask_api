093811e1a53bc3268f9080ff2a1954b8aab806a5
SuvorovJenya <e.s.suvorov@mail.ru>
2022-06-09 19:25:38 +0300

    elimination of comments

--- autos.py
+++ autos.py
@@ -22,30 +22,40 @@ def get_db():
 
 
 @router.get("/autos", response_model=List[schemas.AutoModel])
-def read_autos(
+def get_autos(
         db: Session = Depends(get_db),
+        model: Optional[str] = None,
+        year_of_production: Optional[int] = None,
+        mileage: Optional[str] = None,
+        body_type: Optional[str] = None,
         color: Optional[str] = None,
+        price: Optional[int] = None,
         ):
-    if color:
-        return crud.get_autos(db, color)
-    else:
-        return crud.get_autos(db)
+    return crud.get(
+        db,
+        model=model,
+        year_of_production=year_of_production,
+        mileage=mileage,
+        body_type=body_type,
+        color=color,
+        price=price,
+        )
 
 
 @router.get("/autos/{auto_id}", response_model=schemas.AutoModel)
-def read_auto_one(
+def get_by_id(
         auto_id: int,
         db: Session = Depends(get_db),
         ):
-    db_user = crud.get_autos_one(db, auto_id=auto_id)
+    db_user = crud.get_by_id(db, auto_id=auto_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
 @router.post("/autos")
-def create_item_for_auto(
+def create(
         item: schemas.AutoModel,
         db: Session = Depends(get_db),
         ):
-    return crud.create_auto(db=db, item=item)
+    return crud.create(db=db, item=item)
--- crud.py
+++ crud.py
@@ -3,20 +3,50 @@ import models
 import schemas
 
 
-def get_autos(db: Session, color=None):
+def get(
+    db: Session,
+    model=None,
+    year_of_production=None,
+    mileage=None,
+    body_type=None,
+    color=None,
+    price=None,
+        ):
+    all_filters = []
+    if model:
+        all_filters.append(
+            models.AutoModel.model == model,
+            )
+    if year_of_production:
+        all_filters.append(
+            models.AutoModel.year_of_production == year_of_production,
+            )
+    if mileage:
+        all_filters.append(
+            models.AutoModel.mileage == mileage,
+            )
+    if body_type:
+        all_filters.append(
+            models.AutoModel.body_type == body_type,
+            )
     if color:
-        return db.query(models.AutoModel).\
-            filter(models.AutoModel.color == color).all()
-    else:
-        return db.query(models.AutoModel).all()
+        all_filters.append(
+            models.AutoModel.color == color,
+            )
+    if price:
+        all_filters.append(
+            models.AutoModel.price == price,
+            )
+    return db.query(models.AutoModel).\
+        filter(*all_filters).all()
 
 
-def get_autos_one(db: Session, auto_id: int):
+def get_by_id(db: Session, auto_id: int):
     return db.query(models.AutoModel).\
         filter(models.AutoModel.id == auto_id).first()
 
 
-def create_auto(db: Session, item: schemas.AutoModel):
+def create(db: Session, item: schemas.AutoModel):
     db_item = models.AutoModel(**item.dict())
     db.add(db_item)
     db.commit()
